9f1fc85496801c0ecf645f9f9c7c3684edc2d964
mayor-slash <50599738+mayor-slash@users.noreply.github.com>
2020-04-15 04:51:45 +0200

    Update snake_game.py

--- snake_game.py
+++ snake_game.py
@@ -7,20 +7,22 @@ Created on Wed Apr 15 04:19:09 2020
 import pygame
 
 
-WIN_WIDTH = 800
+WIN_WIDTH = 800 
 WIN_HEIGHT = 600
 
-screen = pygame.display.set_mode((WIN_WIDTH,WIN_HEIGHT))
-pygame.display.set_caption("Snake Remake")
-clock = pygame.time.Clock()
+screen = pygame.display.set_mode((WIN_WIDTH,WIN_HEIGHT)) #initialize the game screen
+pygame.display.set_caption("Snake Remake") #sets Game-Window Label
+clock = pygame.time.Clock() #initalize the game clock
 
 play= True
 
 while play:
     
-    clock.tick(30)
-    for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+    clock.tick(30) #Sets max frame-rate to 30
+    
+    
+    for event in pygame.event.get(): #collects all user input events
+            if event.type == pygame.QUIT: # exits game when mouse clicking exit
                 play = False
                 pygame.quit()
                 break
