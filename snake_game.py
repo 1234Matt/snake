bf0abf79a28dfc8fbd1c78fbd5e337814702fcb5
matej <drbohlav.matej@gmail.com>
2020-04-15 05:33:45 +0200

    adding gamefile matrix

--- snake_game.py
+++ snake_game.py
@@ -5,23 +5,30 @@ Created on Wed Apr 15 04:19:09 2020
 @author: 1234matt & mayor-slash
 """
 import pygame
+import numpy as np
 
 
 WIN_WIDTH = 800 
 WIN_HEIGHT = 600
 
-screen = pygame.display.set_mode((WIN_WIDTH,WIN_HEIGHT)) #initialize the game screen
-pygame.display.set_caption("Snake Remake") #sets Game-Window Label
-clock = pygame.time.Clock() #initalize the game clock
+screen = pygame.display.set_mode((WIN_WIDTH,WIN_HEIGHT)) # initialize the game screen
+pygame.display.set_caption("Snake Remake") # sets Game-Window Label
+clock = pygame.time.Clock() # initalize the game clock
 
-play= True
+play = True
+
+number_of_columns = 24
+number_of_rows = 18
+
+# creating gamefield matrix
+game_field = np.zeros((number_of_columns, number_of_rows))
 
 while play:
     
-    clock.tick(30) #Sets max frame-rate to 30
+    clock.tick(30) # Sets max frame-rate to 30
     
     
-    for event in pygame.event.get(): #collects all user input events
+    for event in pygame.event.get(): # collects all user input events
             if event.type == pygame.QUIT: # exits game when mouse clicking exit
                 play = False
                 pygame.quit()
