58f810a3e8a81dfeb441f8799f1fda48a5e05881
mayor-slash <50599738+mayor-slash@users.noreply.github.com>
2020-04-17 21:12:55 +0200

    Update snake_as_game_class.py

--- snake_as_game_class.py
+++ snake_as_game_class.py
@@ -12,9 +12,14 @@ import random
 WIN_WIDTH = 800 
 WIN_HEIGHT = 600
 box_size = 15
-number_of_columns = int(WIN_WIDTH/box_size)
-number_of_rows = int(WIN_HEIGHT/box_size)
-
+play_field_width = WIN_WIDTH-40
+play_field_height= WIN_HEIGHT-100
+number_of_columns = int(play_field_width/box_size)
+number_of_rows = int(play_field_height/box_size)
+game_field_x0 = 20
+game_field_y0 = 80
+game_field_x1 = game_field_x0 + box_size*(number_of_columns-1)
+game_field_y1 = game_field_y0 + box_size*(number_of_rows-1)
 
 
 
@@ -22,6 +27,7 @@ number_of_rows = int(WIN_HEIGHT/box_size)
 screen = pygame.display.set_mode((WIN_WIDTH,WIN_HEIGHT)) # initialize the game screen
 pygame.display.set_caption("Snake Remake") # sets Game-Window Label
 clock = pygame.time.Clock() # initalize the game clock
+pygame.init()
 
 play = True
 
@@ -45,6 +51,25 @@ index_tuple = {1:(255, 255, 255),
                }
 
 
+#make textboxes
+font = pygame.font.Font(None,80)
+font_color = index_tuple[1]
+font_background = index_tuple[0]
+#Gen = font.render("Generation: "+ str(gen),True,font_color)
+#Alive = font.render("Alive: "+str(50),True,font_color)
+Score = font.render("Score: ",True,font_color)
+
+#Gen_rect = Gen.get_rect()
+#Alive_rect = Alive.get_rect()
+Score_rect = Score.get_rect()
+
+#Gen_rect.top = 50
+#Gen_rect.left = 50
+#Alive_rect.top = 120
+#Alive_rect.left = 50
+Score_rect.top = 5
+Score_rect.left = 5
+
 # 4 different directions the snake can move, by pressing left/right arrow key you can cycle through this directions
 directions = [
     np.array((1,0)),
@@ -62,23 +87,24 @@ class Game:
         self.game_field = np.zeros((number_of_rows, number_of_columns))
         self.snake = Snake()
         self.item = Item(self.snake.snake_position,self.snake.snake_body)
+        self.score = 0
         
     #displays the current game_field onto the screen
     def update_grid(self,screen):
-        y = 0
+        y = game_field_y0
         for row in self.game_field:
-            x = 0
+            x = game_field_x0
             for box in row:
                 rect = pygame.Rect(x, y, box_size, box_size)
                 color = index_tuple[box]
                 pygame.draw.rect(screen,color,rect)
                 x += box_size
             y += box_size
-        pygame.display.update()
+        
         
         
     #main method which will be called inside the main-loop
-    def play(self,screen):
+    def play(self,screen,display_game=False):
         # clears the entire game_field to zeros (makes everything black)
         self.game_field = np.zeros((number_of_rows, number_of_columns))
         
@@ -88,6 +114,7 @@ class Game:
         #create new item if last was found
         if item_found:
             self.item = Item(self.snake.snake_position,self.snake.snake_body)
+            self.score += 1
         
         #draw item onto game_field
         self.draw_item()
@@ -95,23 +122,41 @@ class Game:
         # displays the snake body (the places where the snake has been earliere)
         self.game_field = self.snake.pass_body(self.game_field)
         
-        # draws everything
-        self.update_grid(screen)
-        
-        
+        if display_game:
+            
+            # draws grid 
+            self.update_grid(screen)
+            
+            #draws frame of play_field
+            self.draw_game_field(screen)
+            
+            # update screen
+            pygame.display.update()
         
     def draw_item(self):
         self.game_field[self.item.position[0]][self.item.position[1]]=self.item.color
-
-
+        
+    def draw_game_field(self,screen):
+        pygame.draw.line(screen,index_tuple[1],(game_field_x0,game_field_y0),(game_field_x1,game_field_y0),2)
+        pygame.draw.line(screen,index_tuple[1],(game_field_x1,game_field_y0),(game_field_x1,game_field_y1),2)
+        pygame.draw.line(screen,index_tuple[1],(game_field_x1,game_field_y1),(game_field_x0,game_field_y1),2)
+        pygame.draw.line(screen,index_tuple[1],(game_field_x0,game_field_y1),(game_field_x0,game_field_y0),2)
+        
+        #Gen = font.render("Generation: "+ str(gen),True,font_color)
+        #Alive = font.render("Alive: "+str(len(players)),True,font_color)
+        Score = font.render("Score: "+str(self.score),True,font_color)
+        
+        #screen.blit(Gen,Gen_rect)
+        #screen.blit(Alive,Alive_rect)
+        screen.blit(Score,Score_rect)
 #creating class Item 
 class Item:
     
     def __init__(self,snake_position,snake_body):
         found = False
         while not found:
-            x = random.randint(0,number_of_rows-1)
-            y = random.randint(0,number_of_columns-1)
+            x = random.randint(0,number_of_rows-2)
+            y = random.randint(0,number_of_columns-2)
             d = np.sqrt((x-snake_position[0])**2 + (y - snake_position[1])**2)
             
             if d>5:
@@ -140,11 +185,12 @@ class Snake:
         self.snake_body_color = color_index[color_body] # color of the snake_body is standart white, but could be changed by calling Snake object with extra string arguments
         self.snake_body = [] # empty list where the previous positions of the head and therefor the body is stored
         self.dead = False
+        self.frames_per_movement = 10
         
     def move(self,time,game_field,item_position):
         # only moves snake head every 20 timesteps. Could definitly use a better solution here!
         item_found = False
-        if time%10 ==0:  
+        if time%self.frames_per_movement ==0:  
             #creates copy of snake_positions 
             old = self.snake_position.copy()
             
@@ -205,6 +251,9 @@ class Snake:
         if np.array_equal(self.snake_position,item_position):
             self.snake_length +=1
             item_found = True
+            self.frames_per_movement -=1
+            if self.frames_per_movement <1:
+                self.frames_per_movement=1
             
         return item_found
             
@@ -223,6 +272,7 @@ while play and not game1.snake.dead:
     
     clock.tick(30) # Sets max frame-rate to 30
     time += 1 # increases timestep by 33.33 ms due to the frame_rate
+    screen.fill(index_tuple[0])
     
     
     
@@ -241,7 +291,7 @@ while play and not game1.snake.dead:
                     # changes direction by adding 1 to it. using "%4" to make 4 = 0 
                     game1.snake.snake_direction = (game1.snake.snake_direction +1)%4
     
-    game1.play(screen)
+    game1.play(screen,True)
 
 try:
     pygame.quit()
